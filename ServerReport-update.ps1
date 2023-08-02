c01e29631ba8be6f9721227ba1da49972717508a
DSav-23 <david.chibuzo@outlook.com>
2023-08-02 14:07:07 -0400

    update

--- ServerReport-update.ps1
+++ ServerReport-update.ps1
@@ -1,22 +1,26 @@
 # Refer to ServerReport.ps1 for more info on below script
-
-# Retrieve system name (Dynamic assignment: Variable subject to change)
-$Name = $env:COMPUTERNAME
-$Version = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
-$DiskCount = (Get-CimInstance CIM_LogicalDisk).count 
-$FreeSpace = (Get-CimInstance CIM_LogicalDisk | Where-Object {$_.DeviceID -eq 'C:'}).FreeSpace
-
-
-# Make a custom object
-$obj = [PSCustomObject]@{
-    ComputerName = $Name  #preset value - using stored variable
-    OsVersion = $Version
-    DiskCount = $DiskCount
-    'GB Free' = [System.Math]::Round($FreeSpace / 1Gb, 2) ## Add a class from C# (Compiler for PS) to perform math functions. Divide diskspace by 1gb and round to 2 decimal places
+function Get-PowerSmell {
+    param (
+        $DriveLetter
+    )
+    # Retrieve system name (Dynamic assignment: Variable subject to change)
+    $Version = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
+    $Name = $env:COMPUTERNAME
+    $DiskInstance = Get-CimInstance CIM_LogicalDisk
+    $DiskCount = $DiskInstance.count 
+    $FreeSpace = ($DiskInstance | Where-Object {$_.DeviceID -eq $DriveLetter}).FreeSpace
+    
+    
+    # Make a custom object
+    $obj = [PSCustomObject]@{
+        ComputerName = $Name  #preset value - using stored variable
+        OsVersion = $Version
+        DiskCount = $DiskCount
+        'GB Free' = [System.Math]::Round($FreeSpace / 1Gb, 2) ## Add a class from C# (Compiler for PS) to perform math functions. Divide diskspace by 1gb and round to 2 decimal places
+        
+    }
+    
+    # Show Output
+    Write-Output $obj
     
 }
-
-# Show Output
-Write-Output $obj
-
-
